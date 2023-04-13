0eef35ace1377f3cc0b4a40c2fa83d3e14a790ab
laurodm <laurodamasceno@gmail.com>
2023-04-13 09:14:27 -0300

    export DatalistContext

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
+export { DatalistContext } from "./DataList";
 export { default as DataList } from "./DataList";
 export type { TListPagedParams } from "./types/IApiService";
 export type { TDataListDataFormat, TDatalistColumn } from "./types";
