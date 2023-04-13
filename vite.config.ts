c32f2cc4e095224dbe86edd224e84cddacca5ce3
laurodm <laurodamasceno@gmail.com>
2023-04-12 21:49:29 -0300

    fix vite build

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/lib/index.ts"),
-      name: "@ldm-react/datalist",
+      name: "@ldm-react-datalist",
       formats: ["es", "umd"],
       fileName: (format) => `ldm-react-datalist.${format}.js`,
     },
